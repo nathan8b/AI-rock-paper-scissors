fbc7748572037f00898e74ee43d2bb518cb47967
nathan8b <nb803622@ohio.edu>
2025-02-05 16:18:04 -0500

    Added playRound() function plays one round of game replays round if game is a tie

--- game.js
+++ game.js
@@ -8,14 +8,38 @@ function getComputerChoice() {
         return "paper";
     }
     else if(num === 3){
-        return "scissors"
+        return "scissors";
     }
 }
 
 function getHumanChoice() {
-    return prompt("Enter 'rock' 'paper' or 'scissors': ");
+    return prompt("Enter 'rock' 'paper' or 'scissors': ").toLowerCase();
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+    console.log("Your choice: " + humanChoice + "   Computer choice: " + computerChoice);
+    if(humanChoice === computerChoice){
+        console.log("Tie! Replay this round!");
+        humanChoice = getHumanChoice();
+        computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+    else if((humanChoice === "rock" && computerChoice === "scissors") || 
+        (humanChoice === "paper" && computerChoice === "rock") || 
+        (humanChoice === "scissors" && computerChoice === "paper")) {
+        console.log("You won this round!");
+        humanScore++;
+    }
+    else {
+        console.log("Computer won this round!");
+        computerScore++;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
