24d756a753ecff61507b7819d975fcedcb281261
nathan8b <nb803622@ohio.edu>
2025-02-09 18:06:38 -0500

    started working on ui added buttons to interact with game started working on logic to make game work game is playable now with buttons, need to make it look nice

--- game.js
+++ game.js
@@ -13,51 +13,91 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    return prompt("Enter 'rock' 'paper' or 'scissors': ").toLowerCase();
+    let choice = "";
+    // .forEach iterates through each button
+    buttons.forEach((button) => {
+        // for each, add an event listener
+        button.addEventListener("click", () => {
+            console.log(button.className.toString());
+            choice = button.className.toString();
+        })
+    })
+    return choice;
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
 function playRound(humanChoice, computerChoice) {
+    // print out players moves
+    moves.textContent = humanChoice + " vs " + computerChoice;
+    results.appendChild(moves);
     console.log("Your choice: " + humanChoice + "   Computer choice: " + computerChoice);
+    // if round is a tie
     if(humanChoice === computerChoice){
         console.log("Tie! Replay this round!");
-        humanChoice = getHumanChoice();
-        computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
+        return;
     }
+    // if human wins
     else if((humanChoice === "rock" && computerChoice === "scissors") || 
         (humanChoice === "paper" && computerChoice === "rock") || 
         (humanChoice === "scissors" && computerChoice === "paper")) {
         console.log("You won this round!");
         humanScore++;
-        console.log("Score: " + humanScore + " - " + computerScore);   
+        console.log("Score: " + humanScore + " - " + computerScore); 
     }
+    // if computer wins
     else {
         console.log("Computer won this round!");
         computerScore++;
         console.log("Score: " + humanScore + " - " + computerScore);
     }
-
+    // print out score
+    score.textContent = "Score: " + humanScore + " - " + computerScore;
+    results.appendChild(score);
 }
 
-function playGame() {
-    console.log("Welcome to Rock Paper Scissors! You'll be playing 5 rounds against the computer.");
-    for(let i = 1; i <= 5; i++){
-        console.log("Round " + i + "!");
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-
-        playRound(humanSelection, computerSelection);
-    }
-
-    if(humanScore > computerScore){
-        console.log("You Win!");
-    }
-    else {
-        console.log("Computer Wins!");
-    }
+//plays game until player or computer reaches 5 points
+function playGame(){
+    humanScore = 0;
+    computerScore = 0;
+    
+    // start next round by clicking an option
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            if(humanScore < 5 && computerScore < 5) {
+                playRound(button.className, getComputerChoice());
+            }
+            else {
+                if (humanScore > computerScore) {
+                    console.log("🎉 You Win the Game!");
+                } else {
+                    console.log("💻 Computer Wins the Game!");
+                }
+            }
+        })
+    })
 }
 
-playGame();
+// grab play button
+const playBtn = document.querySelector(".play-btn");
+
+// buttons div
+const buttonsBox = document.getElementById("buttons-box"); // grab parent div for buttons
+const buttons = buttonsBox.querySelectorAll("button"); // grab all buttons to a NodeList
+
+//results div
+const results = document.querySelector("#result"); // grab result box
+
+// create score counter
+const score = document.createElement("p");
+score.classList.add("score-counter");
+
+// create player move display
+const moves = document.createElement("p");
+moves.classList.add("moves-display");
+
+let humanScore = 0;
+let computerScore = 0;
+
+playBtn.addEventListener("click", () => {
+    playBtn.remove();
+    playGame();
+});
